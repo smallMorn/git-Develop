61845b304470713c1bde36f611483434b8d4b94b
mingxinagliu <1939952007@qq.com>
2019-02-22 16:14:13 +0800

    我的构造写好了（明祥爹爹）

--- src/main/java/com/baizhi/entity/Programmer.java
+++ src/main/java/com/baizhi/entity/Programmer.java
@@ -1,5 +1,7 @@
 package com.baizhi.entity;
 
+import java.util.Objects;
+
 public class Programmer {
     /**
      * 深圳写属性
@@ -7,6 +9,7 @@ public class Programmer {
     private Integer id;
     private String name;
     private Double salary;
+
     /**
      * 明祥写构造
      */
@@ -24,46 +27,49 @@ public class Programmer {
     public int hashCode() {
         return Objects.hash(id, name, salary);
     }
-    public Programmer (){}
-    public Programmer(Integer id,String name,Double salary){
+
+    public Programmer() {
+    }
+
+    public Programmer(Integer id, String name, Double salary) {
         super();
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
-
-
-    /**
-     * 阳哥重写equals()
-     */
-
-    /**
-     * 高茂恒写get set  不能使用快捷键
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
         this.salary = salary;
     }
-/**
-     * 科蜜酌情补充
-     */
 
-}
+        /**
+         * 阳哥重写equals()
+         */
+
+        /**
+         * 高茂恒写get set  不能使用快捷键
+         */
+        public Integer getId () {
+            return id;
+        }
+
+        public void setId (Integer id){
+            this.id = id;
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public void setName (String name){
+            this.name = name;
+        }
+
+        public Double getSalary () {
+            return salary;
+        }
+
+        public void setSalary (Double salary){
+            this.salary = salary;
+        }
+/**
+ * 科蜜酌情补充
+ */
+
+    }
