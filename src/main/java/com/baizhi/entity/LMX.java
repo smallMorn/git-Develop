74c451780e06b7da7dda05f76a34591dce431e93
mingxinagliu <1939952007@qq.com>
2019-02-22 16:41:41 +0800

    我的实体类

---
+++ src/main/java/com/baizhi/entity/LMX.java
@@ -0,0 +1,21 @@
+package com.baizhi.entity;
+
+public class LMX {
+
+    private String name = "爸爸";
+
+    public LMX() {
+    }
+
+    public LMX(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
