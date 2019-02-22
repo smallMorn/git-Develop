e88dd3bd89eadcc1c033c3de97e25545fc5bcafc
ssz <857463435@qq.com>
2019-02-22 15:41:56 +0800

    五人小组协同开发

---
+++ src/main/java/com/baizhi/entity/Programmer.java
@@ -0,0 +1,27 @@
+package com.baizhi.entity;
+
+public class Programmer {
+    /**
+     * 深圳写属性
+     */
+
+
+    /**
+     * 明祥写构造
+     */
+
+
+    /**
+     * 阳哥重写equals()
+     */
+
+    /**
+     * 高茂恒写get set  不能使用快捷键
+     */
+
+
+    /**
+     * 科蜜酌情补充
+     */
+
+}
